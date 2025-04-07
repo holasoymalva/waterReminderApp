2a9d1fcbbaf17c3c2680bf0045c983d00b796a12
holasoymalva <alquimista3301@gmail.com>
2025-04-06 22:30:47 -0600

    Initial Commit

---
+++ waterReminderApp Watch App/waterReminderAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  waterReminderAppApp.swift
+//  waterReminderApp Watch App
+//
+//  Created by malva on 06/04/25.
+//
+
+import SwiftUI
+
+@main
+struct waterReminderApp_Watch_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ waterReminderApp Watch AppTests/waterReminderApp_Watch_AppTests.swift
@@ -0,0 +1,17 @@
+//
+//  waterReminderApp_Watch_AppTests.swift
+//  waterReminderApp Watch AppTests
+//
+//  Created by malva on 06/04/25.
+//
+
+import Testing
+@testable import waterReminderApp_Watch_App
+
+struct waterReminderApp_Watch_AppTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
